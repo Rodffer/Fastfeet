add0f606e6dca68bb9b3864b2f09535536079af0
Rodffer <r.rodffer@gmail.com>
2020-03-04 22:40:13 -0300

    Autenticação/JWT/Dados de Entrada Admin

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,17 @@
 import { Router } from 'express';
 
-// import UserController from './app/controllers/UserController';
-// import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
-// import authMiddleware from './app/middlewares/auth';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-// routes.post('/users', UserController.store);
-// routes.post('/sessions', SessionController.store);
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
-// routes.use(authMiddleware);
+routes.use(authMiddleware);
 
-// routes.put('/users', UserController.update);
+routes.put('/users', UserController.update);
 
 export default routes;
